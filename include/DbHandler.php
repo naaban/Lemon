784dfc6a4a224a618bd75abcb9b5616dc835f904
Padmanaban <nabanharish@gmail.com>
2018-09-10 00:00:35 +0530

    full source

---
+++ include/DbHandler.php
@@ -0,0 +1,222 @@
+<?php
+
+class DbHandler {
+ 
+    private $conn;
+ 
+    function __construct() {
+        require_once dirname(__FILE__) . './DbConnect.php';
+        // opening db connection
+        $db = new DbConnect();
+        $this->conn = $db->connect();
+    }
+
+      /**
+     * Creating new user
+     * @param String $name User full name
+     * @param String $email User login email id
+     * @param String $password User login password
+     */ 
+    public function createUser($name, $email, $password) {
+
+        require_once 'PassHash.php';
+        $response = array();
+
+        if(!this->isUserExists($email)){
+
+            // Generating password hash
+            $password_hash = PassHash::hash($password);
+
+            //Generating api key
+            $api_key = $this->generateApiKey();
+
+            //insertQury
+            $stmt = $this->conn->prepare("INSERT INTO user_details(name , email , password_hash ,apikey , status ) VALUES ( ? ,? ,? , ?, 1)");
+            $stmt = $this->bind_params("ssss",$name , $email , $password_hash ,$api_key);
+
+            $result = $stmt->execute();
+
+            $stmt->close();
+
+            if($result){
+
+                return USER_CREATED_SUCCESSFULLY;
+            }
+            else{
+                return USER_CREATE_FAILED;
+            }
+        }
+        else{
+            return USER_ALREADY_EXISTED;
+        }
+        return $response;
+
+        }
+
+
+   /**
+     * Checking user login
+     * @param String $email User login email id
+     * @param String $password User login password
+     * @return boolean User login status success/fail
+     */
+    public function checkLogin($email, $password) {
+        // fetching user by email
+        $stmt = $this->conn->prepare("SELECT password_hash FROM user_details WHERE email = ?");
+ 
+        $stmt->bind_param("s", $email);
+ 
+        $stmt->execute();
+ 
+        $stmt->bind_result($password_hash);
+ 
+        $stmt->store_result();
+ 
+        if ($stmt->num_rows > 0) {
+            // Found user with the email
+            // Now verify the password
+ 
+            $stmt->fetch();
+ 
+            $stmt->close();
+ 
+            if (PassHash::check_password($password_hash, $password)) {
+                // User password is correct
+                return TRUE;
+            } else {
+                // user password is incorrect
+                return FALSE;
+            }
+        } else {
+            $stmt->close();
+ 
+            // user not existed with the email
+            return FALSE;
+        }
+    }
+ 
+    /**
+     * Checking for duplicate user by email address
+     * @param String $email email to check in db
+     * @return boolean
+     */
+    private function isUserExists($email) {
+        $stmt = $this->conn->prepare("SELECT id from user_details WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt->store_result();
+        $num_rows = $stmt->num_rows;
+        $stmt->close();
+        return $num_rows > 0;
+    }
+
+     /**
+     * Fetching user by email
+     * @param String $email User email id
+     */
+    public function getUserByEmail($email) {
+        $stmt = $this->conn->prepare("SELECT name, email, api_key, status, created_at FROM user_details WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        if ($stmt->execute()) {
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+            return $user;
+        } else {
+            return NULL;
+        }
+    }
+ 
+    /**
+     * Fetching user api key
+     * @param String $user_id user id primary key in user table
+     */
+    public function getApiKeyById($user_id) {
+        $stmt = $this->conn->prepare("SELECT api_key FROM user_details WHERE id = ?");
+        $stmt->bind_param("i", $user_id);
+        if ($stmt->execute()) {
+            $api_key = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+            return $api_key;
+        } else {
+            return NULL;
+        }
+    }
+ 
+    /**
+     * Fetching user id by api key
+     * @param String $api_key user api key
+     */
+    public function getUserId($api_key) {
+        $stmt = $this->conn->prepare("SELECT id FROM user_details WHERE api_key = ?");
+        $stmt->bind_param("s", $api_key);
+        if ($stmt->execute()) {
+            $user_id = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+            return $user_id;
+        } else {
+            return NULL;
+        }
+    }
+ 
+    /**
+     * Validating user api key
+     * If the api key is there in db, it is a valid key
+     * @param String $api_key user api key
+     * @return boolean
+     */
+    public function isValidApiKey($api_key) {
+        $stmt = $this->conn->prepare("SELECT id from user_details WHERE api_key = ?");
+        $stmt->bind_param("s", $api_key);
+        $stmt->execute();
+        $stmt->store_result();
+        $num_rows = $stmt->num_rows;
+        $stmt->close();
+        return $num_rows > 0;
+    }
+ 
+    /**
+     * Generating random Unique MD5 String for user Api key
+     */
+    private function generateApiKey() {
+        return md5(uniqid(rand(), true));
+    }
+
+    /////////////////
+    //////ADMIN//////
+    /////////////////
+    
+    /**
+     * Creation of Product
+     * @param array $data uploaded by admin
+     */
+
+    private function addProduct($data){
+
+        $response = array();
+        for($i=0 ; $i<sizeof($data) ; $i++){
+        $stmt = $this->conn->prepare("INSERT INTO products(product_type , product_name , product_des ,price , instock , coupon_code ) VALUES ( ? ,? ,? , ?, ?)");
+        $stmt = $this->bind_params("ssss",$data['product_type'][$i] , $data['product_name'][$i] , $data['product_des'][$i] ,$data['price'][$i],$data['instock'][$i] , $data['coupon_code'][]);
+        $result = $stmt->execute();
+        if($result){
+            return PRODUCT_CREATED_SUCCESSFULLY ." ". $data['product_name'][$i];
+        }
+        else{
+            return PRODUCT_CREATE_FAILED ." ". $data['product_name'][$i];
+        }
+        }
+        $stmt->close();
+    else{
+        return PRODUCT_ALREADY_EXISTED;
+    }
+    return $response;
+
+    }
+
+    private function getProducts(){
+        $stmt = $this->conn->prepare("SELECT * FROM products");
+        $stmt->execute();
+        $result = $stmt->   
+     }
+
+}
+?>
---
+++ include/config.php
@@ -0,0 +1,11 @@
+<?php
+//Databasr Configuration
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'lemon');
+ 
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+?>
